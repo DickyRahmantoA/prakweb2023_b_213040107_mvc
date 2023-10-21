a57e2461370e3ab0c2e742dbee354e892f0277ff
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-10-21 20:22:01 +0700

    Menambahkan catatan mvc di youtube #4

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,10 +1,12 @@
 <?php 
 
 class About {
-    public function index($nama = 'Sandhika', $pekerjaan = 'Dosen'){
+    public function index($nama = 'Sandhika', $pekerjaan = 'Dosen')
+    {
       echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
     }
-    public function page(){
+    public function page()
+    {
       echo 'About/page';
     }
 }
