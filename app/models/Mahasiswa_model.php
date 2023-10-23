f915e5c8b6673cdbcf0e4c48e8574e6202a6f50b
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-10-23 18:38:13 +0700

    menambahkan catatan mvc di youtube #11

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -38,4 +38,16 @@ class Mahasiswa_model {
 
       return $this->db->rowCount();
   }
+
+  public function hapusDataMahasiswa($id)
+  {
+      $query = "DELETE FROM mahasiswa WHERE id = :id";
+      
+      $this->db->query($query);
+      $this->db->bind('id', $id);
+
+      $this->db->execute();
+
+      return $this->db->rowCount();
+  }
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -14,9 +14,10 @@
       <h3>Daftar Mahasiswa</h3>
         <ul class="list-group">
           <?php foreach( $data['mhs'] as $mhs) : ?>
-          <li class="list-group-item d-flex justify-content-between align-items-start">
+          <li class="list-group-item ">
             <?= $mhs['nama']; ?>
-            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">detail</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge text-bg-danger float-end m-1" onclick="return confirm('yakin?')">hapus</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary float-end m-1">detail</a>
           </li>
           <?php endforeach; ?>
         </ul>
