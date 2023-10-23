1c48adde9b4784cfe3979c15a1889857e43440c6
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-10-23 14:37:42 +0700

    menambahkan catatan mvc di youtube #9

--- app/core/Database.php
+++ app/core/Database.php
@@ -69,5 +69,9 @@ class Database {
         return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
 
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,15 +1,67 @@
-<div class="container mt-5">
+<div class="container mt-3">
   <div class="row">
     <div class="col-6">
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+        Tambah Data Mahasiswa
+      </button>
+      <br><br>
       <h3>Daftar Mahasiswa</h3>
         <ul class="list-group">
           <?php foreach( $data['mhs'] as $mhs) : ?>
           <li class="list-group-item d-flex justify-content-between align-items-start">
             <?= $mhs['nama']; ?>
-            <a href="<?php BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">detail</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">detail</a>
           </li>
           <?php endforeach; ?>
         </ul>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="judulModal">Tambah Data</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL; ?>" method="post"> 
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama">
+          </div>
+
+          <div class="form-group">
+            <label for="nim">Nim</label>
+            <input type="text" class="form-control" id="nim" name="nim">
+          </div>
+
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" class="form-control" id="email" name="email">
+          </div>
+
+          <div class="form-group">
+            <label for="jurusan">Jurusan</label>
+            <select class="form-control" id="jurusan" name="jurusan">
+              <option value="Teknik Informatika">Teknik Informatika</option>
+              <option value="Teknik Mesin">Teknik Mesin</option>
+              <option value="Teknik Industri">Teknik Industri</option>
+              <option value="Teknik Pangan">Teknik Pangan</option>
+              <option value="Teknik Planalogi">Teknik Planalogi</option>
+              <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+            </select>
+          </div>
+        
+        
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+          </form>
+        </div>
     </div>
   </div>
 </div>
